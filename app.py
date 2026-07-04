0d287ebb3663d95acb25ed6360e0bd074fa2b619
cels2999-source <cels2999@gmail.com>
2026-07-04 14:45:47 -0600

    initial commit

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-ï»¿import streamlit as st
+import streamlit as st
 
 st.header('Lanzar una moneda')
 
-st.write('Esta aplicaciÃ³n aÃºn no es funcional. En construcciÃ³n.')
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar')
+
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+
+st.write('Esta aplicación aún no es funcional. En construcción.')
